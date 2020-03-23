b2fff3e5bf41930c34920087caaa4a691c96c42a
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-22 23:10:15 -0700

    finished

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-//main file 
-var server = require("./server"); 
 
-server.start();
+var server = require("./server"); 
+var router = require("./router");
+var requestHandlers = require(".requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+server.start(router.route, handle);
 
--- server.js
+++ server.js
@@ -1,54 +1,96 @@
-//server module file
-
-//add to onRequest() function the logic needed to find out which url path the browser requested
-var http = require("http");
+ var http = require("http");
 var url = require("url");
 
-function start() {
-    function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + "received.");
+function start(route, handle) {
+function onRequest(request, response) {
+var pathname = url.parse(request.url).pathname;
+console.log("Request for " + pathname + " received.");
+route(handle, pathname, response, request);
 
-        route(pathname);
+// request.setEncoding("utf8");
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Witchfinder General is cool");
-        response.end();
-    }
-    http.createServer(onRequest).listen(8888);
-    console.log("Server has started.");
+// request.addListener("data", function(postDataChunk) {
+// postData += postDataChunk;
+// console.log("Received POST data chunk '"+
+// postDataChunk + "'.");
+//  });
+
+//  request.addListener("end", function() {
+//  route(handle, pathname, response, postData);
+//  });
 }
+ http.createServer(onRequest).listen(8888);
 
-exports.start = start;
+console.log("Server has started")
+ }
+
+ exports.start = start;
 
 
 
 
 
+// //server module file
 
-
+// //add to onRequest() function the logic needed to find out which url path the browser requested
 // var http = require("http");
+// var url = require("url");
 
-// function onRequest(request, response) {
-//     console.log("Request received.");
-//     response.writeHead(200, {"Content-Type": "test/plain"});
-//     response.write("Motorhead");
-//     response.end();
+// function start() {
+//     function onRequest(request, response) {
+//         var pathname = url.parse(request.url).pathname;
+        
+//         //console.log("Request for " + pathname + "received.");
+
+//         //route(handle, pathname);
+
+//         response.writeHead(200, {"Content-Type": "text/plain"});
+//         //var content = route(handle, pathname)
+//         //response.write("Witchfinder General is cool");
+//         response.write(body);
+//         response.end();
+//     }
+//     function upload(response) {
+//         console.log("Request handler 'upload' was called.");
+//         response.writeHead(200, {"Content-Type": "text/plain"});
+//         response.write("Hello Upload");
+//         response.end();
+//          }
+        
+//     // http.createServer(onRequest).listen(8888);
+//     // console.log("Server has started.");
 // }
 
-// http.createServer(onRequest).listen(8888);
-
-// console.log("Server has started")
-
+// exports.start = start;
+// exports.upload = upload;
 
 
 
-// var http = require("http"); 
 
-// http.createServer(function(request, response){ 
-// response.writeHead(200, {"Content-Type": "text/plan"});
-// response.write("Judas Priest rocks");
-// response.end();
-// }) .listen(8888);
+
+
+// // var http = require("http");
+
+// // function onRequest(request, response) {
+// //     console.log("Request received.");
+// //     response.writeHead(200, {"Content-Type": "test/plain"});
+// //     response.write("Motorhead");
+// //     response.end();
+// // }
+
+// // http.createServer(onRequest).listen(8888);
+
+// // console.log("Server has started")
+
+
+
+
+// // var http = require("http"); 
+
+// // http.createServer(function(request, response){ 
+// // response.writeHead(200, {"Content-Type": "text/plan"});
+// // response.write("Judas Priest rocks");
+// // response.end();
+// // }) .listen(8888);
 
 

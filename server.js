6741d50bc0390e03d22adca13b9813d1ce70b68f
Anastasia Morozova <morozova.asia@gmail.com>
2020-03-22 19:48:25 -0700

    progress

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 //server module file
 
-var http = require("http"); 
+var http = require("http");
 
-http.createServer(function(request, response){ 
-response.writeHead(200, {"Content-Type": "text/plan"});
-response.write("Judas Priest rocks");
-response.end();
-}) .listen(8888);
+function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "test/plain"});
+    response.write("Motorhead");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started")
+
+
+
+
+// var http = require("http"); 
+
+// http.createServer(function(request, response){ 
+// response.writeHead(200, {"Content-Type": "text/plan"});
+// response.write("Judas Priest rocks");
+// response.end();
+// }) .listen(8888);
+
 
